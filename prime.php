4399551f820ccf2ca2a5edb967339e4156ad9e66
almaksumee719 <almaksumee719@gmail.com>
2021-01-07 15:03:10 +0600

    Bootstrap Project

---
+++ prime.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['submit'])){
+	$isPrime = $_POST['']
+}
+
+
+?>
+<form action="" method="post">
+	<input type="text" name="num" placeholder=""><br>
+	<input type="submit" name="submit" value="check">
+</form>
